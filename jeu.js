00d0646067522b145c52b27a8218ca3133f2b4fc
Yannick Biheul <yannickbiheul@outlook.fr>
2020-04-06 16:23:52 +0200

    Création de la logique du lancer de dé

--- jeu.js
+++ jeu.js
@@ -1,4 +1,4 @@
-/* RÉCUPÉRER LE NOM DU JOUEUR */
+/* VARIABLES JOUEUR */
 
 let getName = document.querySelector("#getName");
 let buttonPlayer = document.querySelector("#votreNom");
@@ -8,6 +8,37 @@ let infoNamePlayer = document.querySelector(".info_namePlayer");
 let infoPvPlayer = document.querySelector(".info_pvPlayer");
 let playerName = "";
 let pvPlayer = 50;
+
+
+/* VARIABLES DEBILE */
+
+let imgDebile = document.querySelector("#imgDebile");
+let buttonDebile = document.querySelector("#debile_img");
+let debileTexte = document.querySelector(".debileTexte");
+let infoNameDebile = document.querySelector(".info_nameDebile");
+let infoPvDebile = document.querySelector(".info_pvDebile");
+let nameDebile = "Le Débile";
+let pvDebile = 50;
+
+
+/* VARIABLES DÉ */
+
+let twiceText = document.querySelector(".texteDe");
+let twice = document.querySelector("#twice");
+
+
+/* VARIABLES RÉSULTATS */
+
+let titreLancerJoueur = document.querySelector(".lancerJoueurTitre");
+let resultLancerJoueur = document.querySelector(".resultLancerJoueur");
+let titreLancerDebile = document.querySelector(".lancerDebileTitre");
+let resultLancerDebile = document.querySelector(".resultLancerDebile");
+let randomPlayer = 0;
+let randomDebile = 0;
+let resultCombat = document.querySelector(".resultCombat");
+
+
+/* RÉCUPÉRER LE NOM DU JOUEUR */
 
 buttonPlayer.addEventListener("click", function() {
     playerName = getName.value;
@@ -26,14 +57,6 @@ buttonPlayer.addEventListener("click", function() {
 
 /* DECOUVRIR LE DEBILE */
 
-let imgDebile = document.querySelector("#imgDebile");
-let buttonDebile = document.querySelector("#debile_img");
-let debileTexte = document.querySelector(".debileTexte");
-let infoNameDebile = document.querySelector(".info_nameDebile");
-let infoPvDebile = document.querySelector(".info_pvDebile");
-let nameDebile = "Le Débile";
-let pvDebile = 50;
-
 buttonDebile.addEventListener("click", function() {
     imgDebile.style.opacity = "1";
     buttonDebile.style.display = "none";
@@ -46,7 +69,32 @@ buttonDebile.addEventListener("click", function() {
 })
 
 
-/* FAIRE APPARAITRE LE DE */
+/* LANCER LE DÉ */
 
-let twiceText = document.querySelector(".texteDe");
-let twice = document.querySelector("#twice");
+twice.addEventListener("click", function() {
+    randomPlayer = Math.floor(Math.random() * 10+ 1);
+    randomDebile = Math.floor(Math.random() * 10 + 1);
+    randomResult = "";
+    titreLancerJoueur.style.opacity = "1";
+    resultLancerJoueur.innerHTML = randomPlayer;
+    resultLancerJoueur.style.opacity = "1";
+    titreLancerDebile.style.opacity = "1";
+    resultLancerDebile.innerHTML = randomDebile;
+    resultLancerDebile.style.opacity = "1";
+    if (randomPlayer > randomDebile) {
+        randomResult = randomPlayer - randomDebile;
+        resultCombat.innerHTML = "Vous retirez " + randomResult + " PV au débile.";
+        resultCombat.style.opacity = "1";
+        pvDebile -= randomResult;
+        infoPvDebile.innerHTML = pvDebile + " PV.";
+    } else if (randomDebile > randomPlayer) {
+        randomResult = randomDebile - randomPlayer;
+        resultCombat.innerHTML = "Le débile vous retire " + randomResult + " PV.";
+        resultCombat.opacity = "1";
+        pvPlayer -= randomResult;
+        infoPvPlayer.innerHTML = pvPlayer + " PV.";
+    } else {
+        resultCombat.innerHTML = "Égalité !";
+        resultCombat.style.opacity = "1";
+    }
+})
